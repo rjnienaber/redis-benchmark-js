87915f34c1745c8927c35bfa894ddab6928a691d
Richard Nienaber <rjnienaber@gmail.com>
2016-05-18 07:44:40 +0100

    added concurrent clients

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
 #!/usr/bin/env ./node_modules/.bin/babel-node
 'use strict';
 
-import Connection from './lib/connection.js';
-import convertToBuffer from './lib/convert_to_buffer.js';
-import StopWatch from './lib/stopwatch.js';
+const p = new Promise(() => 42);
+import Benchmark from './lib/benchmark.js';
 
-const conn = new Connection('127.0.0.1', 6379);
-const stopwatch = new StopWatch();
-
-const totalRequests = 1000000;
-
-const command = convertToBuffer('PING');
-
-let counter = 0;
-const onData = () => {
-  counter++;
-  if (counter == totalRequests) {
-    const duration = stopwatch.stop();
-    console.log('====== PING_INLINE ======');
-    console.log(`  ${totalRequests} requests completed in ${duration} seconds`);    
-    console.log('  1 parallel clients');
-    console.log('  3 bytes payload');
-    console.log('  keep alive: 1');
-    console.log('');
-    console.log(`${(totalRequests / duration).toFixed(2)} requests per second`);
-    
-    conn.disconnect();
-    process.exit();
-  }
-    
-  conn.send(command);
+const options = {
+  host: '127.0.0.1',
+  port: 6379,
+  command: 'PING',
+  requests: 100000,
+  clients: 1
 };
 
-conn.receive(onData);
+const benchmark = new Benchmark(options);
+benchmark.run(stopwatch => {
+  console.log('====== PING_INLINE ======');
+  console.log(`  ${options.requests} requests completed in ${stopwatch.duration()} seconds`);    
+  console.log(`  ${options.clients} concurrent clients`);
+  console.log('  3 bytes payload');
+  console.log('  keep alive: 1');
+  console.log('');
+  console.log(`${(options.requests / stopwatch.duration()).toFixed(2)} requests per second`);
 
-stopwatch.start();
-conn.send(command);
+  process.exit(0);
+})
 
 //wait for key
 process.stdin.on('data', buffer => {
   // exit on '\n' or '\r'
   if ([10, 13].includes(buffer[buffer.length - 1])) {
-    conn.disconnect();
+    // conn.disconnect();
     process.exit();
   }
 });
